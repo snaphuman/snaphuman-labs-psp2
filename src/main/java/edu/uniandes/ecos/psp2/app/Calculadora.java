ff77a7935207b7a443f0816f1de7c2d25ebf481d
snaphuman <fhernandezn@gmail.com>
2016-03-02 11:59:20 -0500

    [review] agrega datos de entrada para calculos en test requeridos

--- src/main/java/edu/uniandes/ecos/psp2/app/Calculadora.java
+++ src/main/java/edu/uniandes/ecos/psp2/app/Calculadora.java
@@ -177,7 +177,58 @@ public class Calculadora {
         lista.listaPares.add( new Pair<Double, Double>( 945.0, 1890.0 ) );
         lista.listaPares.add( new Pair<Double, Double>( 368.0, 788.0 ) );
 
-        EstimacionTamano.probeA( lista );
+        Estimacion.probe( lista );
+    }
+
+    public static void test2 () {
+
+        Pares lista = new Pares();
+
+        lista.listaPares.add( Pair.with( 130.0, 15.0 ) );
+        lista.listaPares.add( Pair.with( 650.0, 69.9 ) );
+        lista.listaPares.add( Pair.with( 99.0, 6.5 ) );
+        lista.listaPares.add( Pair.with( 150.0, 22.4 ) );
+        lista.listaPares.add( Pair.with( 128.0, 28.4 ) );
+        lista.listaPares.add( Pair.with( 302.0, 65.9 ) );
+        lista.listaPares.add( Pair.with( 95.0, 19.4 ) );
+        lista.listaPares.add( Pair.with( 945.0, 198.7 ) );
+        lista.listaPares.add( Pair.with( 368.0, 38.8 ) );
+
+        Estimacion.probe( lista );
+    }
+
+    public static void test3 () {
+
+        Pares lista = new Pares ();
+
+        lista.listaPares.add( Pair.with( 163.0, 186.0 ) );
+        lista.listaPares.add( Pair.with( 765.0, 699.0 ) );
+        lista.listaPares.add( Pair.with( 141.0, 132.0 ) );
+        lista.listaPares.add( Pair.with( 166.0, 272.0 ) );
+        lista.listaPares.add( Pair.with( 137.0, 291.0 ) );
+        lista.listaPares.add( Pair.with( 335.0, 331.0 ) );
+        lista.listaPares.add( Pair.with( 136.0, 199.0 ) );
+        lista.listaPares.add( Pair.with( 1206.0, 189.0 ) );
+        lista.listaPares.add( Pair.with( 433.0, 788.0 ) );
+
+        Estimacion.probe( lista );
+    }
+
+    public static void test4 () {
+
+        Pares lista = new Pares ();
+
+        lista.listaPares.add( Pair.with( 163.0, 15.0 ) );
+        lista.listaPares.add( Pair.with( 765.0, 69.9 ) );
+        lista.listaPares.add( Pair.with( 141.0, 6.5 ) );
+        lista.listaPares.add( Pair.with( 166.0, 22.4 ) );
+        lista.listaPares.add( Pair.with( 137.0, 28.4 ) );
+        lista.listaPares.add( Pair.with( 335.0, 65.9 ) );
+        lista.listaPares.add( Pair.with( 136.0, 19.4 ) );
+        lista.listaPares.add( Pair.with( 1206.0, 198.7 ) );
+        lista.listaPares.add( Pair.with( 433.0, 38.8 ) );
+
+        Estimacion.probe( lista );
     }
 
 }
--- src/main/java/edu/uniandes/ecos/psp2/app/Estimacion.java
+++ src/main/java/edu/uniandes/ecos/psp2/app/Estimacion.java
@@ -5,22 +5,28 @@ import org.javatuples.Pair;
 /**
  * Created by snaphuman on 2/28/16.
  */
-public class EstimacionTamano {
+public class Estimacion {
 
     public static Pares probeA(Pares lista) {
 
         System.out.println("Calculando estimación de tamaño probe A");
         Pares coeficientesCorrelacion = new Pares();
+        Pares parametrosRegresion = new Pares();
 
         Correlacion correlacion = new Operacion();
+        Regresion regresion = new Operacion();
 
         for ( Pair item : lista.listaPares) {
 
-            Pair<Double, Double> rCorrelacion = correlacion.calcularCoeficientes((
-                    Double) item.getValue(0), (Double) item.getValue(1));
+            Pair<Double, Double> rCorrelacion = correlacion.calcularCoeficientes(
+                    (Double) item.getValue(0), (Double) item.getValue(1));
 
             coeficientesCorrelacion.listaPares.add(rCorrelacion);
 
+            Pair<Double, Double> rRegresion = regresion.calcularParametros(
+                    (Double) item.getValue(0), (Double) item.getValue(1));
+
+            parametrosRegresion.listaPares.add(rRegresion);
         }
 
         return lista;
