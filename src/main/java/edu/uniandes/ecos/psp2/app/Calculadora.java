812f9dea62e6261837a0dd7111d229927dbeaab9
snaphuman <fhernandezn@gmail.com>
2016-03-02 12:25:39 -0500

    [review] define dependencias para servidor de aplicaciones web

--- src/main/java/edu/uniandes/ecos/psp2/app/Calculadora.java
+++ src/main/java/edu/uniandes/ecos/psp2/app/Calculadora.java
@@ -23,6 +23,8 @@ public class Calculadora {
      */
     public static void main(String[] args) {
 
+        get("/", (req, res) -> "Calculadora estadística");
+
         // Composición del mensaje de bienvenida
         String $msg;
 
