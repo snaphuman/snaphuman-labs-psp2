47404edc900345ebb3451c330716d377dc00fcc9
snaphuman <fhernandezn@gmail.com>
2016-03-03 17:58:19 -0500

    [fix] define el puerto en la app

--- src/main/java/edu/uniandes/ecos/psp2/app/Main.java
+++ src/main/java/edu/uniandes/ecos/psp2/app/Main.java
@@ -32,6 +32,7 @@ public class Main {
      */
     public static void main( String[] args ) {
 
+        port(Integer.valueOf(System.getenv("PORT")));
         staticFileLocation( "/public" );
 
         test1();
