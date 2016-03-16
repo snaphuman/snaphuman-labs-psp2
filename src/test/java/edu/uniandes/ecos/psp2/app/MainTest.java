4e65d8daf554cb19231cc48c60d4279fb89df468
snaphuman <fhernandezn@gmail.com>
2016-03-16 00:09:27 -0500

    [fix] verifica que todas las interfaces estén funcionando

--- src/test/java/edu/uniandes/ecos/psp2/app/MainTest.java
+++ src/test/java/edu/uniandes/ecos/psp2/app/MainTest.java
@@ -3,6 +3,9 @@ package edu.uniandes.ecos.psp2.app;
 import org.javatuples.Pair;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by snaphuman on 2/2/16.
  */
@@ -12,14 +15,18 @@ public class MainTest {
     public void deberiaDemostrarInterfaces() {
 
         Pares lista = new Pares();
-
         lista.listaPares.add(Pair.with( 1.0,1.0 ) );
         lista.listaPares.add(Pair.with( 1.0,1.0 ) );
 
-        Operacion operacion = new Estadistica();
-        operacion.calcular( lista );
+        List<Double> datos = new ArrayList<>();
+        Double varianza = new Double( 0 );
 
         Media media = new Estadistica();
         media.calcularMediaPares( lista );
+
+        Operacion operacion = new Estadistica();
+        operacion.calcular( lista );
+        operacion.calcularDesviacionEstandard( varianza );
+        operacion.calcularVarianza(datos, media.calcularMediaLista(datos));
     }
 }
