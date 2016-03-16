c8539eeb15d16206106b4d77fa38d1f0e6b737f5
snaphuman <fhernandezn@gmail.com>
2016-03-16 00:11:36 -0500

    [fix] verifica que los métodos de calculos retornen el valor y tipo correcto

--- src/test/java/edu/uniandes/ecos/psp2/app/EstadisticaTest.java
+++ src/test/java/edu/uniandes/ecos/psp2/app/EstadisticaTest.java
@@ -3,6 +3,9 @@ package edu.uniandes.ecos.psp2.app;
 import org.javatuples.Pair;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -13,15 +16,32 @@ public class EstadisticaTest {
     private final static double precision = 0.05;
 
     @Test
-    public void calcularMediaAritmetica() {
+    public void calcularMediaPares() {
 
         Pares lista = new Pares();
         Media operacion = new Estadistica();
 
-        lista.listaParesMedia.add( Pair.with( 1.0,1.0 ) );
-        lista.listaParesMedia.add( Pair.with(1.0,1.0 ) );
+        lista.listaPares.add( Pair.with( 1.0,1.0 ) );
+        lista.listaPares.add( Pair.with( 1.0,1.0 ) );
 
-        Pair<Double, Double> result = operacion.calcularMediaPares( lista );
-        assertEquals( 1.0, result.getValue1(), precision );
+        Pair<Double, Double> result = operacion.calcularMediaPares(lista);
+        assertEquals( 1.0, result.getValue0().doubleValue(), precision );
+        assertEquals( 1.0, result.getValue1().doubleValue(), precision );
     }
+
+    @Test
+    public void calcularMediaLista() {
+
+        List<Double> lista = new ArrayList<>();
+        Media operacion = new Estadistica();
+
+        lista.add( 1.0 );
+        lista.add( 1.0 );
+
+        Double result = operacion.calcularMediaLista(lista);
+        assertEquals( 1.0, result, precision );
+    }
+
+
+
 }
